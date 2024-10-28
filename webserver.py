6721e4290b6afce7d97f69ad6e896b6ca90968e9
aarontenzing <aarontenzing@gmail.com>
2024-10-28 17:25:09 +0100

    working app

--- webserver.py
+++ webserver.py
@@ -8,6 +8,7 @@ from matplotlib.offsetbox import OffsetImage, AnnotationBbox
 from PIL import Image
 import numpy as np
 import os
+import sys
 import torch
 from torchvision.models import resnet50
 from torchvision import transforms
@@ -19,10 +20,12 @@ import json
 app = Flask(__name__)
 
 model = None
-EMBEDDINGS = [] # Store the embeddings of the frames
+embeddings = [] # Store the embeddings of the frames
+length_intial_embeddings = 0
 CHECKPOINT_PATH = 'models/resnet50_weights_best_acc.tar'  # Path to the PyTorch checkpoint file
 NUM_CLASSES = 1081  # Number of classes for the Pl@ntNet-300K dataset
 UPLOAD_FOLDER = 'frames' # Folder to store the uploaded frames
+toggle_samples = False
 
 if not os.path.exists(UPLOAD_FOLDER):
     os.makedirs(UPLOAD_FOLDER)
@@ -31,7 +34,8 @@ def encoder(model, input_tensor):
     model.eval()  # Set the model to evaluation mode
     with torch.no_grad():
         embedding = model(input_tensor)  # Get features from the encoder
-    return embedding
+
+    return embedding.cpu().numpy().flatten()
 
 def load_model():
     # Initialize model
@@ -76,29 +80,54 @@ def load_img(img_path, target_size=None):
         img = img.resize(target_size)
     return np.array(img)  # Convert to a numpy array for matplotlib
         
-def visualize_embeddings():
-    features = tsne.fit_transform(EMBEDDINGS)  # Fit the t-SNE model to the embeddings
-    
-    files = os.listdir(UPLOAD_FOLDER)
+def visualize_embeddings(embeddings, samples_vis=False):
+    print("Visualize embedding")
+    embeddings = np.array(embeddings)
+    features = tsne.fit_transform(embeddings)  # Fit the t-SNE model to the embeddings
+    embeddings = embeddings.tolist()
+
     # Create a scatter plot
     plt.figure(figsize=(10, 8))
     
+    if samples_vis:
+        samples = sorted(os.listdir("samples"), key=lambda x: int(x.split('.')[0]))
+
+        # Loop through each point and add the corresponding thumbnail
+        for idx, point in enumerate(features[:length_intial_embeddings]):
+                x, y = point[0], point[1]
+                    
+                plt.scatter(x, y, alpha=0)  # Hide the original points
+                    
+                # Load and create thumbnail from images in samples directory
+                img = load_img(os.path.join("samples", samples[idx]), target_size=(50, 50))  # Resize thumbnail
+                imagebox = OffsetImage(img, zoom=0.8)  # Adjust zoom as needed
+                
+                # Create an annotation box with the thumbnail
+                ab = AnnotationBbox(imagebox, (x, y), frameon=False, pad=0.1)
+                
+                # Add the annotation box to the plot
+                plt.gca().add_artist(ab)
+
+    # Do for captured frames    
+    frames = sorted(os.listdir(UPLOAD_FOLDER), key=lambda x: int(x.split('.')[0]))
+    print(frames)
+
     # Loop through each point and add the corresponding thumbnail
-    for idx, point in enumerate(features):
-        x, y = point[0], point[1]
+    for idx, point in enumerate(features[length_intial_embeddings:]):
+            x, y = point[0], point[1]
+                
+            plt.scatter(x, y, alpha=0)  # Hide the original points
+                
+            # Load and create thumbnail for the image
+            img = load_img(os.path.join("frames", frames[idx]), target_size=(50, 50))  # Resize thumbnail
+            imagebox = OffsetImage(img, zoom=0.8)  # Adjust zoom as needed
             
-        plt.scatter(x, y, alpha=0)  # Hide the original points
+            # Create an annotation box with the thumbnail
+            ab = AnnotationBbox(imagebox, (x, y), frameon=False, pad=0.1)
             
-        # Load and create thumbnail for the image
-        img = load_img(os.path.join('images', files[idx]), target_size=(50, 50))  # Resize thumbnail
-        imagebox = OffsetImage(img, zoom=0.8)  # Adjust zoom as needed
+            # Add the annotation box to the plot
+            plt.gca().add_artist(ab)
         
-        # Create an annotation box with the thumbnail
-        ab = AnnotationBbox(imagebox, (x, y), frameon=False, pad=0.1)
-        
-        # Add the annotation box to the plot
-        plt.gca().add_artist(ab)
-    
     plt.title("Image Embeddings")
     plt.xlabel("Dimension 1")
     plt.ylabel("Dimension 2")
@@ -108,40 +137,65 @@ def visualize_embeddings():
     plot_image_path = os.path.join('static/images', 'embeddings_plot.png')  # Save in the static folder
     plt.savefig(plot_image_path)
     plt.close()
+
+    return
         
 @app.route("/")
 def home():
     return render_template("index.html")
 
+@app.route("/toggle", methods=['POST'])
+def toggle():
+    global toggle_samples
+    global embeddings
+    toggle_samples = not toggle_samples
+    print(f"Toggled: is_on is now {toggle_samples}")
+    visualize_embeddings(embeddings, samples_vis=toggle_samples)
+    return redirect(url_for('home'))   
+
+
 @app.route('/upload', methods=['POST', 'GET'])
 def plot():
+    global toggle_samples
+    global embeddings
     if 'frame' not in request.files:
-        return "No file part", 400  # Returns an error if 'frame' is not in files
+        print("No file part") # Returns an error if 'frame' is not in files
     else: 
         frame = request.files['frame']
-        filename = secure_filename(frame.filename)
+        filename = str(len(embeddings)-length_intial_embeddings) + ".jpg" 
         file_path = os.path.join(UPLOAD_FOLDER, filename)
         frame.save(file_path)
         frame = Image.open(file_path)
         frame = preprocess_image(frame)
         embedding = encoder(model, frame)
-        EMBEDDINGS.append(embedding) # Pass frame (image) through the PlantNet network and retrieve the embedding
+        embeddings.append(embedding) # Pass frame (image) through the PlantNet network and retrieve the embedding
 
-    visualize_embeddings() 
+    visualize_embeddings(embeddings, samples_vis=toggle_samples) 
     
     return redirect(url_for('home'))       
-    
+
+@app.route('/reset', methods=['POST'])
+def reset_embeddings():
+    # Remove embeddings from list
+    # Clear directory of frames
+    pass
 
 if __name__ == "__main__":
     
     model = load_model() # Load the PlantNet model and weights
     
-    tsne = TSNE(n_components=2, perplexity=25, random_state=42) # Initialize the t-SNE model
-    
+    tsne = TSNE(n_components=2, perplexity=25, random_state=42, max_iter=1000)
+
+    # Load initial datapoints voor TSNE
     with open("embeddings.json", 'r') as json_file:
         json_data = json.load(json_file)
     
-    EMBEDDINGS = np.array([item['feature'] for item in json_data])  # Convert features to NumPy array
+    embeddings = [item['feature'] for item in json_data] 
+    length_intial_embeddings = len(embeddings)
 
+    size = sys.getsizeof(embeddings)
+    print("Total size list in bytes:", size)
+
+    visualize_embeddings(embeddings, samples_vis=False) 
     
     app.run(debug=True)
